9102a69f4f6d3e1786bcb3eba39f8c508b3357d5
Kristiyan Tsaklev <cuki@cukii.tk>
2016-12-25 21:54:42 +0200

    Apply fft over data

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include "stream.h"
+#include "fft.hpp"
 
-#include<stdio.h>
+#include<iostream>
 
-short buffer[256];
+const int SAMPLE_COUNT = 1 << 15;
+
+short buffer[SAMPLE_COUNT];
 
 int main() {
 	open_stream();
 	read_stream(buffer, sizeof(buffer));
 	free_stream();
+
+	std::vector<std::complex<double>> data(SAMPLE_COUNT);
+	for(int i = 0; i < SAMPLE_COUNT; ++i)
+		data[i] = (double) buffer[i];
+
+	fft(data);
+	for(int i = 0; i < SAMPLE_COUNT / 2; ++i)
+		std::cout << abs(data[i]) / SAMPLE_COUNT << '\n';
 }
