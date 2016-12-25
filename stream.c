3f0f820b6743c41f1d7700e590f7906d28ad72ed
Kristiyan Tsaklev <cuki@cukii.tk>
2016-12-25 19:26:20 +0200

    Split pulseaudio functions

---
+++ stream.c
@@ -0,0 +1,31 @@
+#include "stream.h"
+
+#include<pulse/simple.h>
+
+static pa_simple *s;
+static pa_sample_spec ss = {
+	.format = PA_SAMPLE_S16NE,
+	.channels = 1,
+	.rate = 44100
+};
+
+void open_stream(void) {
+	s = pa_simple_new(NULL,
+			"Test",
+			PA_STREAM_RECORD,
+			NULL,
+			"Music",
+			&ss,
+			NULL,
+			NULL,
+			NULL
+			);
+}
+
+void read_stream(void *buffer, int buf_size) {
+	pa_simple_read(s, buffer, buf_size, NULL);
+}
+
+void free_stream(void) {
+	if(s) pa_simple_free(s);
+}
---
+++ stream.h
@@ -0,0 +1,8 @@
+#ifndef __AMPLITUDE_H
+#define __AMPLITUDE_H
+
+void open_stream(void);
+void read_stream(void*, int);
+void free_stream(void);
+
+#endif
