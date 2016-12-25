22a306ce572e9eb56c3bf333f2cd8e6f9038db2c
Kristiyan Tsaklev <cuki@cukii.tk>
2016-12-26 00:36:18 +0200

    Add sample count regulation

--- main.cpp
+++ main.cpp
@@ -6,37 +6,42 @@
 #include<vector>
 #include<complex>
 
-const int SAMPLE_COUNT = 1 << 15;
-
-short buffer[SAMPLE_COUNT];
+const int MIN_SAMPLE_COUNT = 32;
+const int MAX_SAMPLE_COUNT = 1 << 16;
 
 const int FREQUENCY_CHANGE_STEP = 100;
 const int THRESHOLD_CHANGE_STEP = 100;
+
+const int SAMPLE_RATE = 44100;
+
+short buffer[MAX_SAMPLE_COUNT];
 
 int main() {
 	initscr();
 	raw();
 	noecho();
 
-	std::vector<std::complex<double>> data(SAMPLE_COUNT);
-
 	int min_frequency = 0;
 	int max_frequency = 20000;
 	int threshold = 1000;
+	int sample_count = MIN_SAMPLE_COUNT;
+
+	std::vector<std::complex<double>> data(sample_count);
 
 	bool running = true;
 	while(running) {
 		clear();
 		printw("Threshold: %d\n", threshold);
 		printw("Frequency: %d - %d Hz\n", min_frequency, max_frequency);
+		printw("Sample count: %d\n", sample_count);
 		printw("====================\n");
 		refresh();
 
-		for(int i = 0; i < SAMPLE_COUNT / 2; ++i) {
-			double freq = 44100.0F / SAMPLE_COUNT * i;
+		for(int i = 0; i < sample_count / 2; ++i) {
+			double freq = (double) SAMPLE_RATE / sample_count * i;
 			if(freq < min_frequency || freq > max_frequency)
 				continue;
-			double amp = abs(data[i]) * 2 / SAMPLE_COUNT;
+			double amp = abs(data[i]) * 2 / sample_count;
 			if(amp < threshold)
 				continue;
 			printw("%lfHz\n", freq);
@@ -49,7 +54,7 @@ int main() {
 
 			case 's': // make a sample
 				attron(A_BOLD);
-				mvprintw(2, 3, " Sampling ... ");
+				mvprintw(3, 3, " Sampling ... ");
 				attroff(A_BOLD);
 				refresh();
 
@@ -57,7 +62,7 @@ int main() {
 				read_stream(buffer, sizeof(buffer));
 				free_stream();
 
-				for(int i = 0; i < SAMPLE_COUNT; ++i)
+				for(int i = 0; i < sample_count; ++i)
 					data[i] = (double) buffer[i];
 				fft(data);
 
@@ -86,6 +91,19 @@ int main() {
 				break;
 			case ']':
 				threshold += THRESHOLD_CHANGE_STEP;
+				break;
+
+			case 'w':
+				if(sample_count > MIN_SAMPLE_COUNT) {
+					sample_count >>= 1;
+					data.resize(sample_count);
+				}
+				break;
+			case 'e':
+				if(sample_count < MAX_SAMPLE_COUNT) {
+					sample_count <<= 1;
+					data.resize(sample_count);
+				}
 				break;
 		}
 	}
